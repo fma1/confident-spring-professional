2fce53035f0762299dc5ed7fb4abc155432ab169
Fred Ma <fredma1234@gmail.com>
2022-02-27 10:45:29 -0500

    Modified index.html to show username and current date Added model and username parameter to WebsiteController#homepage()

--- mod4-myfancypdfinvoices/src/main/scala/com/marcobehler/myfancypdfinvoices/web/WebsiteController.scala
+++ mod4-myfancypdfinvoices/src/main/scala/com/marcobehler/myfancypdfinvoices/web/WebsiteController.scala
@@ -1,13 +1,18 @@
 package com.marcobehler.myfancypdfinvoices.web
 
 import org.springframework.stereotype.Controller
-import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.ui.Model
+import org.springframework.web.bind.annotation.{GetMapping, RequestParam}
+
+import java.util.Date
 
 @Controller
 class WebsiteController {
 
   @GetMapping(Array("/"))
-  def homepage(): String = {
+  def homepage(model: Model, @RequestParam(required = false, defaultValue = "stranger") username: String): String = {
+    model.addAttribute("username", username)
+    model.addAttribute("currentDate", new Date())
     "index.html"
   }
 }
